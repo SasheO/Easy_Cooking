0460fd60fa916e62fd5160f710aac4027ec438fc
Mezisashe Ojuba <sasheojuba@fb.com>
2022-07-28 15:24:23 -0700

    populated how to use page

--- app/src/main/java/com/example/groceriesmanager/Fragments/HowToUseFragment.java
+++ app/src/main/java/com/example/groceriesmanager/Fragments/HowToUseFragment.java
@@ -1,10 +1,13 @@
 package com.example.groceriesmanager.Fragments;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
@@ -37,7 +40,19 @@ public class HowToUseFragment extends DialogFragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         // Get field from view
-//        mEditText = (EditText) view.findViewById(R.id.txt_your_name);
+        VideoView vvSwitchList = view.findViewById(R.id.vvSwitchList);
+        vvSwitchList.setVideoURI(Uri.parse("android.resource://" + getContext().getPackageName() +"/"+R.raw.how_to_use_switch_list));
+        vvSwitchList.setMediaController(new MediaController(getContext()));
+        vvSwitchList.requestFocus();
+        vvSwitchList.start();
+
+        VideoView vvEditItem = view.findViewById(R.id.vvEditItem);
+        vvEditItem.setVideoURI(Uri.parse("android.resource://" + getContext().getPackageName() +"/"+R.raw.how_to_use_edit_item));
+        vvEditItem.setMediaController(new MediaController(getContext()));
+        vvEditItem.requestFocus();
+        vvEditItem.start();
+
+
         // Fetch arguments from bundle and set title
         String title = getArguments().getString("title", "How To Use");
         getDialog().setTitle(title);
